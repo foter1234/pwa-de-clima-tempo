17d8bba4e3e9d9aeb50cfe80232d34f465d3104a
Lucas Pereira Esteves <lucaspesteves2017@gmail.com>
2023-11-30 14:55:02 -0400

    termino do projeto clima tempo

--- js/main.js
+++ js/main.js
@@ -12,6 +12,10 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+//3e49a9f061362db6a52d3f6eae67fe1c
+//https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}
+
+const chaveapi = "3e49a9f061362db6a52d3f6eae67fe1c";
 let posicaoInicial;
 
 
@@ -21,36 +25,87 @@ const longitude = document.getElementById('longitude');
 const mapa = document.getElementById('mapa');
 
 
-const sucesso = (posicao) => {
 
-posicaoInicial =posicao;
 
-latitude.innerHTML = posicaoInicial.coords.latitude;
-longitude.innerHTML = posicaoInicial.coords.longitude;
-mapa.src = `https://maps.google.com/maps?q=${posicaoInicial.coords.latitude},${posicaoInicial.coords.longitude}&t=&z=13&ie=UTF8&iwloc=&output=embed`
+const cidade = document.querySelector("#local")
+const botao = document.querySelector("#capturaClima")
+const cidadeElemento = document.querySelector("#city")
+const temperaturaElemento = document.querySelector("#temperatura")
+const descricaoElemento = document.querySelector("#descricao")
+const umidadeElemento = document.querySelector("#umidade")
+
+
+
+const climaLocalizacao = async(posicao)=>{
+  posicaoInicial =posicao;
+
+  latitude.innerHTML = posicaoInicial.coords.latitude;
+  longitude.innerHTML = posicaoInicial.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${posicaoInicial.coords.latitude}&lon=${posicaoInicial.coords.longitude}&units=metric&appid=${chaveapi}&lang=pt_br`
+ 
+  const res= await fetch(url)
+  const data = await res.json()
+  
+  return data
 
 }
+
+const cidadeInput = async(city)=>{
+ 
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${chaveapi}&lang=pt_br`
+
+  const res= await fetch(url)
+  const data = await res.json()
+  
+  return data
+}
+
+const buscacidade = async (city)=>{
+ const data= await cidadeInput(city)
+
+ cidadeElemento.innerText = data.name
+ temperaturaElemento.innerText = parseInt(data.main.temp)
+ descricaoElemento.innerText = data.weather[0].description
+ umidadeElemento.innerText = parseInt(data.main.humidity)
+
+}
+
+const buscaPosicao= async (posicao)=>{
+
+  const data= await climaLocalizacao(posicao)
+ 
+  cidadeElemento.innerText = data.name
+  temperaturaElemento.innerText = parseInt(data.main.temp)
+  descricaoElemento.innerText = data.weather[0].description
+  umidadeElemento.innerText = parseInt(data.main.humidity)
+ }
 
 const erro = (error) =>{
 
-let errorMessage
+  let errorMessage
+  
+  switch (error.code) {
+    case 0:
+   errorMessage="erro desconhecido"
+    break;
+    case 1:
+   errorMessage="permissão negada!"
+    break;
+    case 2:
+   errorMessage="captura Desconhecida!"
+    break;
+    case 3:
+   errorMessage="Tempo de solicitação excedido!"
+    break;
+  }
+  console.log("ocorreu um erro:"+ errorMessage)
+  }
 
-switch (error.code) {
-  case 0:
- errorMessage="erro desconhecido"
-  break;
-  case 1:
- errorMessage="permissão negada!"
-  break;
-  case 2:
- errorMessage="captura Desconhecida!"
-  break;
-  case 3:
- errorMessage="Tempo de solicitação excedido!"
-  break;
-}
-console.log("ocorreu um erro:"+ errorMessage)
-}
+
+botao.addEventListener("click", (e) =>{
+const city =cidade.value
+buscacidade(city)
+})
 capturarLocalizacao.addEventListener('click', () => {
-navigator.geolocation.getCurrentPosition(sucesso, erro)
+  navigator.geolocation.getCurrentPosition(buscaPosicao, erro)
 })
